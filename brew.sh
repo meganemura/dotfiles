4c842ffff9fa276a4360b4a668762bc44711d62f
meganemura <mura2megane@gmail.com>
2014-08-14 16:16:13 +0900

    brew: Rename to `brew.sh`. (Brewfile is unsupported)

---
+++ brew.sh
@@ -0,0 +1,28 @@
+brew update
+brew upgrade
+
+brew install autoconf
+brew install bash-completion
+brew install fontconfig
+brew install fontforge
+brew install freetype
+brew install gettext
+brew install git
+brew install hub
+brew install jpeg
+brew install libevent
+brew install libpng
+brew install libtiff
+brew install lv
+brew install md5sha1sum
+brew install mongodb
+brew install node
+brew install openssl
+brew install pkg-config
+brew install rbenv
+brew install readline
+brew install reattach-to-user-namespace
+brew install ruby-build
+brew install tig
+brew install tmux
+brew install wget
