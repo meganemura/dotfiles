533c1270579ae9d2e503e2e2f6f4f5dbcfbd4f21
meganemura <mura2megane@gmail.com>
2014-08-14 16:16:31 +0900

    brew: Update and add brew-casks

--- brew.sh
+++ brew.sh
@@ -1,28 +1,19 @@
 brew update
 brew upgrade
 
-brew install autoconf
 brew install bash-completion
-brew install fontconfig
-brew install fontforge
-brew install freetype
-brew install gettext
 brew install git
-brew install hub
-brew install jpeg
-brew install libevent
-brew install libpng
-brew install libtiff
 brew install lv
-brew install md5sha1sum
-brew install mongodb
-brew install node
-brew install openssl
-brew install pkg-config
 brew install rbenv
 brew install readline
-brew install reattach-to-user-namespace
 brew install ruby-build
-brew install tig
 brew install tmux
+brew install vim --with-lua
 brew install wget
+
+# Homebrew Cask
+brew install caskroom/cask/brew-cask
+
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install iterm2
