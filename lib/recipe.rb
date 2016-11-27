7cb4250ad00194c47eb90c6835a13e15af51e30d
meganemura <meganemura@users.noreply.github.com>
2016-11-28 02:12:37 +0900

    Install brew first

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,9 +1,10 @@
 root_dir = File.expand_path('../..', __FILE__)
-include_recipe File.join(root_dir, 'cookbooks', 'dotfiles')
-include_recipe File.join(root_dir, 'cookbooks', 'vim')
-include_recipe File.join(root_dir, 'cookbooks', 'ruby')
 
 if node[:platform] == 'darwin'
   include_recipe File.join(root_dir, 'cookbooks', 'mac')
   include_recipe File.join(root_dir, 'cookbooks', 'brew')
 end
+
+include_recipe File.join(root_dir, 'cookbooks', 'dotfiles')
+include_recipe File.join(root_dir, 'cookbooks', 'vim')
+include_recipe File.join(root_dir, 'cookbooks', 'ruby')
