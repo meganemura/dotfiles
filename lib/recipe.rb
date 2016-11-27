b5daaa748c9e681b848cc70b894a2de8950109e2
meganemura <meganemura@users.noreply.github.com>
2016-11-27 21:55:53 +0900

    Add entrypoint

---
+++ lib/recipe.rb
@@ -0,0 +1,8 @@
+root_dir = File.expand_path('../..', __FILE__)
+include_recipe File.join(root_dir, 'cookbooks', 'dotfiles')
+include_recipe File.join(root_dir, 'cookbooks', 'vim')
+include_recipe File.join(root_dir, 'cookbooks', 'ruby')
+
+if node[:platform] == 'darwin'
+  include_recipe File.join(root_dir, 'cookbooks', 'mac')
+end
